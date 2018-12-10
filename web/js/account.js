256858df1d507958491f0f7cdba70853d156a970
Anson <1483809252@qq.com>
2018-12-10 14:32:32 +0800

    interaction logic of login page

--- web/js/account.js
+++ web/js/account.js
@@ -1,7 +1,27 @@
 // 有关账户信息的后台调用
 
 function login(name, password) {
+    if (!name || name === "") {
+        return {
+            success: false,
+            message: "姓名不能为空",
+            data: null
+        };
+    }
 
+    if (!password || password === "") {
+        return {
+            success: false,
+            message: "密码不能为空",
+            data: null
+        };
+    }
+
+    return {
+        success: true,
+        message: null,
+        data: 'MANAGER'
+    };
 }
 
 function logout() {
