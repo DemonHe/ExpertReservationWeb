ccdfea94ce04dc57b971445dbf08f23ef8a788a6
Anson <1483809252@qq.com>
2018-12-14 15:43:36 +0800

    add CROS filter

---
+++ src/main/java/com/service/config/CROSConfiguration.java
@@ -0,0 +1,21 @@
+package com.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CROSConfiguration {
+
+    @Bean
+    public WebMvcConfigurer crosConfigure() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/service/**");
+            }
+        };
+    }
+}
